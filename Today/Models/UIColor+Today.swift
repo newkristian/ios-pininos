c320ebf7ce6b522adb4b7a41057deae517007482
kristianconk <christiansoto6@gmail.com>
2024-07-24 16:59:13 -0600

    primer avance lista con textos y accesorios, pendiente funcionalidad

---
+++ Today/Models/UIColor+Today.swift
@@ -0,0 +1,59 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import UIKit
+
+extension UIColor {
+    static var toodayDetailCellTint: UIColor {
+        UIColor(named: "TodayDetailCellTint") ?? .tintColor
+    }
+
+    static var toodayListCellBackground: UIColor {
+        UIColor(named: "TodayListCellBackground") ?? .secondarySystemBackground
+    }
+
+    static var toodayListCellDoneButtonTint: UIColor {
+        UIColor(named: "TodayListCellDoneButtonTint") ?? .tintColor
+    }
+
+    static var toodayGradientAllBegin: UIColor {
+        UIColor(named: "TodayGradientAllBegin") ?? .systemFill
+    }
+
+    static var toodayGradientAllEnd: UIColor {
+        UIColor(named: "TodayGradientAllEnd") ?? .quaternarySystemFill
+    }
+
+    static var toodayGradientFutureBegin: UIColor {
+        UIColor(named: "TodayGradientFutureBegin") ?? .systemFill
+    }
+
+    static var toodayGradientFutureEnd: UIColor {
+        UIColor(named: "TodayGradientFutureEnd") ?? .quaternarySystemFill
+    }
+
+    static var toodayGradientTodayBegin: UIColor {
+        UIColor(named: "TodayGradientTodayBegin") ?? .systemFill
+    }
+
+    static var toodayGradientTodayEnd: UIColor {
+        UIColor(named: "TodayGradientTodayEnd") ?? .quaternarySystemFill
+    }
+
+    static var toodayNavigationBackground: UIColor {
+        UIColor(named: "TodayNavigationBackground") ?? .secondarySystemBackground
+    }
+
+    static var toodayPrimaryTint: UIColor {
+        UIColor(named: "TodayPrimaryTint") ?? .tintColor
+    }
+
+    static var toodayProgressLowerBackground: UIColor {
+        UIColor(named: "TodayProgressLowerBackground") ?? .systemGray
+    }
+
+    static var toodayProgressUpperBackground: UIColor {
+        UIColor(named: "TodayProgressUpperBackground") ?? .systemGray6
+    }
+}
